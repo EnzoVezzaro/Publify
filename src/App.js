08a345b85cc8e639862d37759779d78d81329357
EnzoVezzaro <enzo-vezzaro@live.it>
2018-02-24 14:55:15 +0100

    routing system

--- src/App.js
+++ src/App.js
@@ -1,27 +1,83 @@
 
-
+// Main Component
 import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
 } from 'react-router-dom';
+// DB
+import firebase from 'firebase';
+import auth from "firebase/auth";
+import database from "firebase/database";
+import firestore from "firebase/firestore";
+// Routing
 import HomePage from './routes/HomePage.js';
 import Dashboard from './routes/Dashboard.js';
 
-const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      database: '',
+      firestore: '',
+      first: '',
+      rootDB: '',
+    }
 
-      <hr/>
+    var config = {
+      apiKey: "AIzaSyALUqJAFbeKcqluUkyg7wwYGzHHWf3pw0w",
+      authDomain: "moviapp-7943b.firebaseapp.com",
+      databaseURL: "https://moviapp-7943b.firebaseio.com",
+      projectId: "moviapp-7943b",
+      storageBucket: "",
+      messagingSenderId: "902800466121"
+    };
 
-      <Route exact path="/" component={HomePage}/>
-      <Route path="/about" component={Dashboard}/>
-    </div>
-  </Router>
-)
+    // Initialize Firebase
+    firebase.initializeApp(config);
+    // Take db to state
+    this.state.firestore = firebase.firestore();
+    this.state.database = firebase.database();
+
+  }
+
+  componentWillMount(){
+
+    let users = firebase.database().ref().child('users/1/')
+
+    let self = this;
+
+    users.on('value', function(snapshot) {
+      // TODO: Ringrazia dado per il self pattern
+      self.setState({
+        first: snapshot.val().first,
+        last: snapshot.val().last,
+      });
+
+    });
+
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">{this.state.first}</Link></li>
+            <li><Link to="/login">About</Link></li>
+            <li><Link to="/signup">{this.state.last}</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" render={() => <HomePage rootDB={this.state.first} />}/>
+          <Route path="/login" component={Dashboard}/>
+          <Route path="/signup" component={Dashboard}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
 export default App;
--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  buttom
 } from 'react-router-dom';
 import logo from './../logo.svg';
 import './../App.css';
 
-class HomePage extends Component {
+class HomePage extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      rootDB: this.props.rootDB
+    }
+  }
+
+  componentDidMount(){
+    //console.log(this.props.rootDB);
+  }
+
+  componentWillReceiveProps(newProps) {
+    // receive props from parent
+    this.setState({
+      rootDB: newProps.rootDB
+    });
+
+  }
+
 
   render() {
     return (
       <div>
-        <h2>HomePage</h2>
+        <h2>{ this.state.rootDB }</h2>
+        <h1>{ this.state.rootDB }</h1>
       </div>
     );
   }
